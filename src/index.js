2b3d00a52f9dc8b0381e4cf2726071842e6b43d7
Teitur Guðmundar <84858035+Siminn-teiturg@users.noreply.github.com>
2021-10-12 12:30:11 +0000

    uuh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import axios from 'axios';
 import Fuse from 'fuse.js';
-import data from './../data/small.json';
+import data from './../data/series.json';
 
 const app = express();
 const PORT = 8080;
@@ -11,7 +10,13 @@ app.listen(
   () => console.log(`it's alive on http://localhost:${PORT}`)
 )
 
-app.get('/movies/:id', (req, res) => {
+app.get('/series', (req, res) => {
+  res.status(200).send({
+    data
+  })
+});
+
+app.get('/series/:id', (req, res) => {
   const { id } = req.params;
   const options = { 
     includeScore: true,
